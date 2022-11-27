37fb8b9567cc313c92f7b2b0018995564df7125e
Ashishmehta1112 <119177610+Ashishmehta1112@users.noreply.github.com>
2022-11-27 19:49:39 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ clrscr();
+    int a=6;
+    
+     int i=0;
+    printf("table of 6 is ");
+    for(i=1;i<11;i++)
+    
+    { 
+      printf(" %d\n ",a*i);
+    printf("%d",a);
+    printf("×");
+    printf ("%d\n :",i);
+    }
+}
